1dcd11b584fc671f8ffeb5524aece2fa9f2f69cb
JustinFung <835776509@qq.com>
2024-06-15 15:37:25 +0800

    C306

---
+++ Chapter 3/6.Loop/loop.s
@@ -0,0 +1,22 @@
+                .text
+                .global calc_sum_asm
+// int calc_sum_asm(int *nums, int n); (r2: current idx, r3: current addr, r4: current val, r5: current sum, r6: sizeof(int))
+calc_sum_asm:   push    {r4-r6}
+                sub     r1,r1,#1
+                mov     r2,#0
+                mov     r3,#0
+                mov     r4,#0       
+                mov     r5,#0       
+                mov     r6,#4           // init
+
+loop:           mul     r3,r2,r6
+                ldr     r4,[r0,r3]
+                add     r5,r4,r5
+                
+                add     r2,r2,#1
+                cmp     r2,r1
+                ble     loop            // Update index, check if index <= (n - 1)
+                                        // Jump back to loop if 
+                mov     r0,r5
+                pop     {r4-r6}
+                bx      lr
---
+++ Chapter 3/6.Loop/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calc_sum_asm(int *nums, int n);
+
+static int calc_sum(int *nums, int n)
+{
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        result += nums[i];
+    }
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) return 0;
+
+    int num_list[argc - 1];
+    for (int i = 1; i < argc; i++) num_list[i - 1] = atoi(argv[i]);
+
+    printf("Calsum: %d\n", calc_sum(num_list, argc - 1));
+    printf("Calsum_asm: %d\n", calc_sum_asm(num_list, argc - 1));
+    return 0;
+}
