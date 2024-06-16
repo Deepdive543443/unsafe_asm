2fa23c8b048caf379cf127578311128c93adf3cc
JustinFung <835776509@qq.com>
2024-06-16 23:30:51 +0800

    c402

--- Chapter 4/2.ArrArithCondd/main.c
+++ Chapter 4/2.ArrArithCondd/main.c
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 int g_max = 50;
 
@@ -12,15 +13,15 @@ static int arr_sum(int *list, int n)
     return result;
 }
 
-extern "C" int arr_sum_asm(int *list, int n);
+int arr_sum_asm(int *list, int n);
 
 int main(int argc, char **argv)
 {
     int n = argc - 1;
     int num_list[n];
     for (int i = 1; i < argc; i++) num_list[i - 1] = atoi(argv[i]);
-    std::cout << arr_sum(num_list, n) << std::endl;
-    std::cout << arr_sum_asm(num_list, n) << std::endl;
+    printf("%d\n", arr_sum(num_list, n));
+    printf("%d\n", arr_sum_asm(num_list, n));
 
     return 0;
 }
