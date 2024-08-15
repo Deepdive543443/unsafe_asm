999a388a7e2313f74f379f396bcaf9c7b9cdbee8
JustinFung <835776509@qq.com>
2024-08-15 22:42:49 +0800

    Zen

--- Chapter10/2.ldr_64/src/main.rs
+++ Chapter10/2.ldr_64/src/main.rs
@@ -2,10 +2,9 @@ mod unsafe_ops;
 use rand::{thread_rng, Rng};
 
 macro_rules! rand_vec {
-    ($type:ty, $len:tt, $min:tt, $max:tt) => {
+    ($type:ty, $len:tt, $min:expr, $max:expr) => {
         {
             let (min, max) = ($min as $type, $max as $type);
-            
             let mut rng = thread_rng();
             let mut vec_rand: Vec<$type> = Vec::new();
             for _ in 0..$len {vec_rand.push(rng.gen_range(min..max))}
@@ -14,21 +13,14 @@ macro_rules! rand_vec {
     };
 }
 
-
 fn main() {
-    let (min, max): (f32,f32) = (-100.0, 100.0);
     let (short_vec, word_vec, quad_vec) = (
-        rand_vec!(i16, 8, min, max),
-        rand_vec!(i32, 8, min, max), 
-        rand_vec!(i64, 8, min, max)
+        rand_vec!(i16, 8, -100.0, 100.0),
+        rand_vec!(i32, 8, -100.0, 100.0), 
+        rand_vec!(i64, 8, -100.0, 100.0)
     );
 
-    let sum: i16 = sum_vec!(short_vec);
-    println!("(Short) Safe:{:8} Unsafe: {:8}", sum, unsafe_ops::sum_short(short_vec));
-
-    let sum: i32 = sum_vec!(word_vec);
-    println!("(Word)  Safe:{:8} Unsafe: {:8}", sum, unsafe_ops::sum_word(word_vec));
-
-    let sum: i64 = sum_vec!(quad_vec);
-    println!("(Quad)  Safe:{:8} Unsafe: {:8}", sum, unsafe_ops::sum_quad(quad_vec));
+    println!("(Short) Safe:{:8} Unsafe: {:8}", sum_vec!(short_vec), unsafe_ops::sum_short(short_vec));
+    println!("(Word)  Safe:{:8} Unsafe: {:8}", sum_vec!(word_vec), unsafe_ops::sum_word(word_vec));
+    println!("(Quad)  Safe:{:8} Unsafe: {:8}", sum_vec!(quad_vec), unsafe_ops::sum_quad(quad_vec));
 }
