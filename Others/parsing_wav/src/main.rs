b0342808b0f6d29b86ad7334d139754f1a1354fe
JustinFung <835776509@qq.com>
2024-10-20 12:24:13 +0800

    Wavheader

--- Others/parsing_wav/src/main.rs
+++ Others/parsing_wav/src/main.rs
@@ -14,30 +14,63 @@ use std::io::prelude::*;
 use nom::{number::complete::{le_u16, le_u32}, IResult};
 use nom::character::complete::anychar;
 
-fn parse_wav_header(input: &[u8]) -> IResult<&[u8], (String, u32, String, u32, u16, u16, u32, u32, u16, u16, String, u32)> {
+struct WavHeader {
+    riff            : String,
+    size            : u32,
+    wavefmt         : String,
+    format_size     : u32,
+    format_type     : u16,
+    num_channels    : u16,
+    sample_rate     : u32,
+    bytes_per_sec   : u32,
+    mono            : u16,
+    bits_per_sample : u16,
+    chunk_mark      : String,
+    data_size       : u32
+}
+
+fn wav_header_init() -> WavHeader {
+    WavHeader {
+        riff            : String::new(),
+        size            : 0,
+        wavefmt         : String::new(),
+        format_size     : 0,
+        format_type     : 0,
+        num_channels    : 0,
+        sample_rate     : 0,
+        bytes_per_sec   : 0,
+        mono            : 0,
+        bits_per_sample : 0,
+        chunk_mark      : String::new(),
+        data_size       : 0
+    }
+}
+
+fn parse_wav_header(input: &[u8]) -> IResult<&[u8], WavHeader> {
+    let mut wav_header = wav_header_init();
     let mut ptr = input;
     let mut temp: char;
 
-    let mut riff       = String::new();
-    let mut wavefmt    = String::new();
-    let mut chunk_mark = String::new();
+    wav_header.riff       = String::new();
+    wav_header.wavefmt    = String::new();
+    wav_header.chunk_mark = String::new();
     
-    for _ in 0..4 { (ptr, temp) = anychar(ptr)?;riff.push(temp); }
-    let size: u32;  (ptr, size) = le_u32(ptr)?;
-    for _ in 0..8 { (ptr, temp) = anychar(ptr)?;wavefmt.push(temp); }
+    for _ in 0..4 { (ptr, temp) = anychar(ptr)?;wav_header.riff.push(temp); }
+    (ptr, wav_header.size) = le_u32(ptr)?;
+    for _ in 0..8 { (ptr, temp) = anychar(ptr)?;wav_header.wavefmt.push(temp); }
     
-    let format_size     : u32; (ptr, format_size)     = le_u32(ptr)?;
-    let format_type     : u16; (ptr, format_type)     = le_u16(ptr)?;
-    let num_channels    : u16; (ptr, num_channels)    = le_u16(ptr)?;
-    let sample_rate     : u32; (ptr, sample_rate)     = le_u32(ptr)?;
-    let bytes_per_sec   : u32; (ptr, bytes_per_sec)   = le_u32(ptr)?;
-    let mono            : u16; (ptr, mono)            = le_u16(ptr)?;
-    let bits_per_sample : u16; (ptr, bits_per_sample) = le_u16(ptr)?;
+    (ptr, wav_header.format_size)     = le_u32(ptr)?;
+    (ptr, wav_header.format_type)     = le_u16(ptr)?;
+    (ptr, wav_header.num_channels)    = le_u16(ptr)?;
+    (ptr, wav_header.sample_rate)     = le_u32(ptr)?;
+    (ptr, wav_header.bytes_per_sec)   = le_u32(ptr)?;
+    (ptr, wav_header.mono)            = le_u16(ptr)?;
+    (ptr, wav_header.bits_per_sample) = le_u16(ptr)?;
     
-    for _ in 0..4     { (ptr, temp)      = anychar(ptr)?;chunk_mark.push(temp); }
-    let data_size: u32; (ptr, data_size) = le_u32(ptr)?;
+    for _ in 0..4     { (ptr, temp)      = anychar(ptr)?;wav_header.chunk_mark.push(temp); }
+    (ptr, wav_header.data_size) = le_u32(ptr)?;
     
-    Ok((ptr, (riff, size, wavefmt, format_size, format_type, num_channels, sample_rate, bytes_per_sec, mono, bits_per_sample, chunk_mark, data_size)))
+    Ok((ptr, wav_header))
 }
 
 fn main() -> std::io::Result<()> {
@@ -50,17 +83,17 @@ fn main() -> std::io::Result<()> {
 
     let (_, wav_header) = parse_wav_header(ptr).unwrap();
 
-    println!("{:<15} : {}", "RIFF"           , wav_header.0);
-    println!("{:<15} : {}", "File size"      , wav_header.1);
-    println!("{:<15} : {}", "wavefmt"        , wav_header.2);
-    println!("{:<15} : {}", "format_size"    , wav_header.3);
-    println!("{:<15} : {}", "format_type"    , wav_header.4);
-    println!("{:<15} : {}", "num_channels"   , wav_header.5);
-    println!("{:<15} : {}", "sample_rate"    , wav_header.6);
-    println!("{:<15} : {}", "bytes_per_sec"  , wav_header.7);
-    println!("{:<15} : {}", "mono"           , wav_header.8);
-    println!("{:<15} : {}", "bits_per_sample", wav_header.9);
-    println!("{:<15} : {}", "chunk_mark"     , wav_header.10);
-    println!("{:<15} : {}", "Data_size"      , wav_header.11);
+    println!("{:<15} : {}", "RIFF"           , wav_header.riff);
+    println!("{:<15} : {}", "File size"      , wav_header.size);
+    println!("{:<15} : {}", "wavefmt"        , wav_header.wavefmt);
+    println!("{:<15} : {}", "format_size"    , wav_header.format_size);
+    println!("{:<15} : {}", "format_type"    , wav_header.format_type);
+    println!("{:<15} : {}", "num_channels"   , wav_header.num_channels);
+    println!("{:<15} : {}", "sample_rate"    , wav_header.sample_rate);
+    println!("{:<15} : {}", "bytes_per_sec"  , wav_header.bytes_per_sec);
+    println!("{:<15} : {}", "mono"           , wav_header.mono);
+    println!("{:<15} : {}", "bits_per_sample", wav_header.bits_per_sample);
+    println!("{:<15} : {}", "chunk_mark"     , wav_header.chunk_mark);
+    println!("{:<15} : {}", "Data_size"      , wav_header.data_size);
     Ok(())
 }
