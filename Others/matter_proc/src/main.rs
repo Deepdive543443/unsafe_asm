2af013484373fff23aed2b7616703814a1087113
JustinFung <835776509@qq.com>
2024-11-18 12:15:41 +0800

    Update Error control

--- Others/matter_proc/src/main.rs
+++ Others/matter_proc/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io;
 mod matter;
 
 #[derive(Parser)]
@@ -22,17 +23,18 @@ struct Args {
     discovery_cap_bitmask: u32,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let mat = matter::new(
+    let matter = matter::new(
         args.vendor_id,
         args.product_id,
         args.passcode,
         args.discriminator,
         args.commissioning_flow,
         args.discovery_cap_bitmask,
-    );
-    println!("Manualcode : {}", mat.gen_manual_code());
-    println!("QRCode     : {}", mat.gen_qr_code());
+    )?;
+    println!("Manualcode : {}", matter.gen_manual_code());
+    println!("QRCode     : {}", matter.gen_qr_code());
+    Ok(())
 }
